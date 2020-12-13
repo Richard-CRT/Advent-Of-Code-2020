ad8ea6030192dca4e10afe85326883d8bf697b7a
Richard Carter <Richard-CRT@users.noreply.github.com>
2020-12-13 20:01:21 +0000

    Day 8-10

---
+++ Day 8 - Handheld Halting/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeUtilities;
+
+namespace Day_8___Handheld_Halting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = AoCUtilities.GetInputLines();
+
+            int acc;
+            Simulate(inputList, out acc);
+            Console.WriteLine(acc);
+
+            bool loop = true;
+            for (int instruction_i = 0; instruction_i < inputList.Count; instruction_i++)
+            {
+                string instruction = inputList[instruction_i];
+                string opcode = instruction.Substring(0, 3);
+                if (opcode == "nop" || opcode == "jmp")
+                {
+                    List<string> copiedInputList = new List<string>(inputList);
+                    if (opcode == "nop")
+                    {
+                        copiedInputList[instruction_i] = instruction.Replace("nop", "jmp");
+                    }
+                    else if (opcode == "jmp")
+                    {
+                        copiedInputList[instruction_i] = instruction.Replace("jmp", "nop");
+                    }
+                    loop = Simulate(copiedInputList, out acc);
+                    if (!loop)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (loop)
+            {
+                throw new ArgumentException("Didn't find an instruction to switch that fixes the error");
+            }
+
+            Console.WriteLine(acc);
+            Console.ReadLine();
+        }
+
+        static bool Simulate(List<string> instructions, out int acc)
+        {
+            int[] instructionExecutionCount = new int[instructions.Count];
+            acc = 0;
+            int pc = 0;
+            while (pc < instructions.Count && instructionExecutionCount[pc] == 0)
+            {
+                instructionExecutionCount[pc]++;
+
+                string instruction = instructions[pc];
+                string[] instructionParts = instruction.Split(' ');
+                string opcode = instructionParts[0];
+                int numericArg = int.Parse(instructionParts[1]);
+
+                switch (opcode)
+                {
+                    case "nop":
+                        pc += 1;
+                        break;
+                    case "acc":
+                        pc += 1;
+                        acc += numericArg;
+                        break;
+                    case "jmp":
+                        pc += numericArg;
+                        break;
+                }
+            }
+
+            return pc < instructions.Count;
+        }
+    }
+}
---
+++ Day 9 - Encoding Error/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeUtilities;
+
+namespace Day_9___Encoding_Error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = AoCUtilities.GetInputLines();
+            List<long> inputNumbers = inputList.ConvertAll(str => long.Parse(str));
+            const int preambleLength = 25;
+            List<long> availableNumbers = new List<long>(preambleLength);
+            for (int i = 0; i < preambleLength; i++)
+            {
+                availableNumbers.Add(inputNumbers[i]);
+            }
+
+            int number_i = preambleLength;
+            for (; number_i < inputNumbers.Count; number_i++)
+            {
+                long targetNumber = inputNumbers[number_i];
+                bool foundSum = false;
+                foreach (long availableNumber1 in availableNumbers)
+                {
+                    foreach (long availableNumber2 in availableNumbers)
+                    {
+                        if (availableNumber1 + availableNumber2 == targetNumber)
+                        {
+                            foundSum = true;
+                            break;
+                        }
+                    }
+                    if (foundSum)
+                        break;
+                }
+                if (foundSum)
+                {
+                    availableNumbers.RemoveAt(0);
+                    availableNumbers.Add(targetNumber);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            long badNumber = inputNumbers[number_i];
+            Console.WriteLine(badNumber);
+
+            List<long> contiguousRange = null;
+            for (int startNumber_i = 0; startNumber_i < inputNumbers.Count - 1; startNumber_i++)
+            {
+                long sum = 0;
+                int contiguousElementCount = 2;
+                while (sum < badNumber)
+                {
+                    sum = inputNumbers.Skip(startNumber_i).Take(contiguousElementCount).Sum();
+                    contiguousElementCount++;
+                }
+                if (sum == badNumber)
+                {
+                    contiguousRange = inputNumbers.Skip(startNumber_i).Take(contiguousElementCount - 1).ToList();
+                }
+            }
+            if (contiguousRange == null)
+            {
+                throw new ArgumentException("No contiguous range is found that sums to the value " + badNumber);
+            }
+            long minInContiguousRange = contiguousRange.Min();
+            long maxInContiguousRange = contiguousRange.Max();
+            long weakness = minInContiguousRange + maxInContiguousRange;
+            Console.WriteLine(weakness);
+            Console.ReadLine();
+        }
+    }
+}
